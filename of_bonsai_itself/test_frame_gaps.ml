ab71321dcdd42b1da4338995dda4a830ec680299
public-release <as-public-release@janestreet.com>
2025-11-20 22:05:17 +0000

    v0.18~preview.130.76+222

--- of_bonsai_itself/test_frame_gaps.ml
+++ of_bonsai_itself/test_frame_gaps.ml
@@ -64,7 +64,7 @@ let%expect_test _ =
   [%expect {| true |}]
 ;;
 
-(* ...but this stops working when multiple other [before_display]s set the state.
+(*=...but this stops working when multiple other [before_display]s set the state.
 
    This example is a bit convoluted, but only because this state is hard to
    get into. The idea is that [state] is set in two different before_display loops in
@@ -84,8 +84,8 @@ let%expect_test _ =
 let component input (local_ graph) =
   (* This is meant to be synced to [input] + 1. *)
   let input_plus_one, set_input_plus_one = Bonsai.state 0 graph in
-  (* The first element is meant to be synced with [input], and the second is meant to
-     be synced with [input_plus_one]. *)
+  (* The first element is meant to be synced with [input], and the second is meant to be
+     synced with [input_plus_one]. *)
   let state, set_state = Bonsai.state' (0, 0) graph in
   (* This is meant to be synced with [state]. *)
   let state_mirror, set_state_mirror = Bonsai.state (0, 0) graph in
@@ -134,15 +134,15 @@ let%expect_test _ =
       (fun (local_ graph) -> component (Bonsai.Expert.Var.value var) graph)
   in
   Handle.show handle;
-  (* Here, the [on_change]s all run at the beginning, so they can't run again
-     to sync with the updated [input_plus_one]. *)
+  (* Here, the [on_change]s all run at the beginning, so they can't run again to sync with
+     the updated [input_plus_one]. *)
   [%expect {| (1 (0 0) (0 0)) |}];
   Handle.show handle;
   [%expect {| (1 (0 1) (0 1)) |}];
   Bonsai.Expert.Var.set var 1;
   Handle.show handle;
-  (* Here, the two on_changes attached to [input] run first, updating [state] and
-     and [input_plus_one].
+  (* Here, the two on_changes attached to [input] run first, updating [state] and and
+     [input_plus_one].
 
      Then, the on_changes attached to each of those run, updating [state_mirror] and
      [state], respectively.
--- of_bonsai_itself/test_time_travel.ml
+++ of_bonsai_itself/test_time_travel.ml
@@ -167,7 +167,7 @@ let%expect_test "now - kind of weird" =
   let handle =
     Handle.create
       (Result_spec.sexp (module Time_ns.Alternate_sexp))
-      (fun (local_ graph) -> Bonsai.Clock.now graph)
+      (fun (local_ graph) -> Bonsai.Clock.Expert.now graph)
   in
   let go n =
     Handle.advance_clock handle ~to_:(seconds n);
